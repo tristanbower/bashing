5cf3f0a9dc379f347e7853b81afe7c46d64d8551
Tristan <tristanobower@gmail.com>
2022-09-14 16:32:36 -0400

    best day ever script

---
+++ bestdayever.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+#echo "What is your name?"
+#read name
+
+name=$1
+compliment=$2
+date=$(date)
+
+echo "good morning $name "
+sleep 1
+echo "You're looking good today $name"
+sleep 1
+echo "You have the best $compliment I've ever seen $name!"
+sleep 2
+echo "Today is $date"
+sleep 1
+echo "Have an amazing day"
+
